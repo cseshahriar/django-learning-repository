b8ddfd188eac84a5392debafa0b6a5a5f369998b
cseshahriar <cses.hahriar.hosen@gmail.com>
2020-03-06 01:42:29 +0600

    Form handle

--- veryfirsrproject/pages/views.py
+++ veryfirsrproject/pages/views.py
@@ -8,6 +8,12 @@ def about(request):
     return render(request, 'about.html',  {'title' : 'About Page Title'})
 
 def contact(request):
+    if(request.method == 'POST'):
+        email = request.POST['email']
+        address = request.POST['address']
+        city = request.POST['city']
+        zipcode = request.POST['zip']
+        return render(request, 'contact.html',  {'title' : 'Contact Page Title', 'email' : email, 'address' : address, 'city' : city, 'zipcode' : zipcode})
     return render(request, 'contact.html',  {'title' : 'Contact Page Title'})
 
 
