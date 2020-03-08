2b3607577e0c22ca97e67f7eab3a6d18fd974552
cseshahriar <cses.hahriar.hosen@gmail.com>
2020-03-09 00:15:41 +0600

    add user models in admin panel

--- firstdjangoproject/posts/models.py
+++ firstdjangoproject/posts/models.py
@@ -1,17 +1,43 @@
 from django.db import models
 from django import forms 
-from django.core import validators 
+from django.core import validators  
 from django.core.validators import ValidationError 
 
 
 # Database Table 
 class Posts(models.Model): 
-    title = models.CharField(max_length=255) 
+    # model level validation
+    def min_length_check(val):
+        if len(val) < 3:
+            raise ValidationError('Title must grater than 2', params={'val':val})
+
+    title = models.CharField(validators=[min_length_check], max_length=255) 
     content = models.TextField()  
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True) 
 
     objects = models.Manager 
+    # admin panel how to display 
+    def __str__(self):
+        return self.title
+
+
+# category
+class Category(models.Model): 
+    # model level validation
+    def min_length_check(val):
+        if len(val) < 3:
+            raise ValidationError('Title must grater than 2', params={'val':val})
+
+    title = models.CharField(validators=[min_length_check], max_length=255)  
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
+
+    objects = models.Manager 
+    # admin panel how to display 
+    def __str__(self):
+        return self.title
+
 
 # Html form 
 class PostsForm(forms.ModelForm):
@@ -28,3 +54,11 @@ class PostsForm(forms.ModelForm):
         if(len(fields['content']) <= 3):
             #%(val)s
             raise validators.ValidationError("Content must be greater than 3", params={'val':keys[1]})
+
+
+
+# Category form 
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['title']
--- firstdjangoproject/posts/views.py
+++ firstdjangoproject/posts/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
-from posts.models import Posts, PostsForm 
+from posts.models import Posts, PostsForm, Category
 # Create your views here.
 
 def index(request):
     form = PostsForm()
     data = Posts.objects.all()
+    categories = Category.objects.all()
     if request.method == 'POST':
         form = PostsForm(request.POST) # all fields data received 
         if form.is_valid():
             form.save()
             return redirect('/') 
-    return render(request, 'index.html', {'title': 'Add New Post', 'form': form, 'data': data})
+    return render(request, 'index.html', {'title': 'Add New Post', 'form': form, 'data': data, 'categories':categories})
