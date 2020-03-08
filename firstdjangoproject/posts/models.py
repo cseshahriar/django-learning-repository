771c079bbbb0d0206f8ce1c7332fe1decb2a1a4b
cseshahriar <cses.hahriar.hosen@gmail.com>
2020-03-08 22:34:01 +0600

    form validation

--- firstdjangoproject/posts/models.py
+++ firstdjangoproject/posts/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 from django import forms 
+from django.core import validators 
+from django.core.validators import ValidationError 
 
-# Create your models here.
+
+# Database Table 
 class Posts(models.Model):
-    title = models.CharField(max_length=255) 
-    content = models.TextField()  
+    def min_length_check(val):
+        if len(val) <= 2:
+            raise validators.ValidationError('Al least 2 character required')
+
+    title = models.CharField(validators=[validators.validate_email],max_length=255) 
+    content = models.TextField(validators=[min_length_check])  
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True) 
 
     objects = models.Manager 
 
+# Html form 
 class PostsForm(forms.ModelForm):
     class Meta:
         model = Posts  
